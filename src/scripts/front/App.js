ce4b352c6ae7a7b3d60710441f3968f6b3c9ee49
akopton <kopton.olek@gmail.com>
2022-10-26 22:49:12 +0200

    initial commit

---
+++ src/scripts/front/App.js
@@ -0,0 +1,10 @@
+import LoginPanel from "./LoginPanel";
+
+const App = () => {
+  return (
+    <div>
+      <LoginPanel />
+    </div>
+  )
+}
+export default App;
---
+++ src/scripts/front/LoginPanel.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const LoginPanel = () => {
+
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(0)
+
+    useEffect(() => {
+        console.log('elo')
+    }, [login])
+    
+    const loginValidation = () => {
+
+    }
+
+
+    return (
+        <div className="login-panel">
+            <input
+                id="login"
+                className="login-panel__login-input --input"
+                type='text'
+                placeholder='login'
+                onChange={(e) => {
+                            setLogin(e.target.value)
+                        }} 
+            ></input>
+            <input 
+                id="password"
+                className="login-panel__password-input --input"
+                type='text'
+                placeholder='password'
+                onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+            />
+            <input 
+                className="login-panel__login-button"
+                type="submit"
+                value="siem mordo"
+                onClick={() =>
+                            loginValidation()
+                        }
+            />
+        </div>
+    )
+}
+
+export default LoginPanel;
