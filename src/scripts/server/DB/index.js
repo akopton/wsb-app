4a60f044469c26dc795e9da6aa1ac6053df5ef76
akopton <kopton.olek@gmail.com>
2023-05-15 23:29:48 +0200

    added env variables

--- src/scripts/server/DB/index.js
+++ src/scripts/server/DB/index.js
@@ -1,156 +1,167 @@
-const mongoose = require('mongoose');
-const { MongoClient } = require('mongodb')
-const uri = 'mongodb+srv://olek:zaqwsxcde@app.t3wuhzm.mongodb.net/?retryWrites=true&w=majority'
-const client = new MongoClient(uri)
-const usersCollection = client.db('wsb_app_database').collection('usersList')
-const tasksCollection = client.db('wsb_app_database').collection('tasksList')
-const generatedId = client.db('wsb_app_database').collection('taskIdGenerator')
-const ObjectId = require('mongodb').ObjectId
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_URL = process.env.DB_URL
 
-async function checkIfUserExists(client, newUser) {
-    try {
-        await client.connect()
-        const result = await usersCollection.findOne({ login: newUser.login })
-        if (result == null) return false
-        else return true
-    } catch (e) {
-        console.error(e)
-    } finally {
-        await client.close()
+if (DB_USERNAME && DB_PASSWORD && DB_URL) {
+    const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}`
+
+    const { MongoClient } = require('mongodb')
+    const client = new MongoClient(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    const usersCollection = client.db('wsb_app_database').collection('usersList')
+    const tasksCollection = client.db('wsb_app_database').collection('tasksList')
+    const generatedId = client.db('wsb_app_database').collection('taskIdGenerator')
+    const ObjectId = require('mongodb').ObjectId
+
+
+
+    async function checkIfUserExists(client, newUser) {
+        try {
+            await client.connect()
+            const result = await usersCollection.findOne({ login: newUser.login })
+            if (result == null) return false
+            else return true
+        } catch (e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
     }
-}
 
-async function getListOfUsers(client) {
-    try {
-        await client.connect()
-        const result = await usersCollection.find({}).toArray()
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        await client.close()
+    async function getListOfUsers(client) {
+        try {
+            await client.connect()
+            const result = await usersCollection.find({}).toArray()
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
     }
-}
 
-async function registerNewUser(client, newUser) {
-    try {
-        await client.connect()
-        const result = await usersCollection.insertOne(newUser)
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        console.log(`New user registered: ${newUser.login}`)
-        await client.close()
+    async function registerNewUser(client, newUser) {
+        try {
+            await client.connect()
+            const result = await usersCollection.insertOne(newUser)
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            console.log(`New user registered: ${newUser.login}`)
+            await client.close()
+        }
     }
-}
 
-async function getListOfTasks(client) {
-    try {
-        await client.connect()
-        const result = await tasksCollection.find({}).toArray()
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        await client.close()
+    async function getListOfTasks(client) {
+        try {
+            await client.connect()
+            const result = await tasksCollection.find({}).toArray()
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
     }
-}
 
-async function addNewTaskToDatabase(client, newTask) {
-    try {
-        await client.connect()
-        await tasksCollection.insertOne(newTask)
-        const result = await tasksCollection.find({}).toArray()
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        console.log('new task added')
-        await client.close()
+    async function addNewTaskToDatabase(client, newTask) {
+        try {
+            await client.connect()
+            await tasksCollection.insertOne(newTask)
+            const result = await tasksCollection.find({}).toArray()
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            console.log('new task added')
+            await client.close()
+        }
     }
-}
 
-async function deleteTask(client, TASK_TO_DELETE) {
-    const { _id } = TASK_TO_DELETE
+    async function deleteTask(client, TASK_TO_DELETE) {
+        const { _id } = TASK_TO_DELETE
 
-    try {
-        await tasksCollection.deleteOne({ "_id": ObjectId(_id) })
-        const result = await tasksCollection.find({}).toArray()
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        console.log(`Task deleted: ${_id}`)
-        await client.close()
+        try {
+            await tasksCollection.deleteOne({ "_id": ObjectId(_id) })
+            const result = await tasksCollection.find({}).toArray()
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            console.log(`Task deleted: ${_id}`)
+            await client.close()
+        }
     }
-}
 
-async function updateTaskStatus(client, UPDATED_TASK) {
-    const { _id, title, description, status } = UPDATED_TASK
+    async function updateTaskStatus(client, UPDATED_TASK) {
+        const { _id, title, description, status } = UPDATED_TASK
 
-    try {
-        await client.connect()
-        await tasksCollection.updateOne({ "_id": ObjectId(_id) }, { $set: { title: title, description: description, status: status } })
-        const result = await tasksCollection.find({}).toArray()
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        console.log(`Task updated: ${_id}`)
-        await client.close()
+        try {
+            await client.connect()
+            await tasksCollection.updateOne({ "_id": ObjectId(_id) }, { $set: { title: title, description: description, status: status } })
+            const result = await tasksCollection.find({}).toArray()
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            console.log(`Task updated: ${_id}`)
+            await client.close()
+        }
     }
-}
 
-async function getIdFromDatabase(client) {
-    try {
-        await client.connect()
-        const result = await generatedId.findOne({})
-        return result
-    } catch (e) {
-        console.log(e)
-    } finally {
-        await client.close()
+    async function getIdFromDatabase(client) {
+        try {
+            await client.connect()
+            const result = await generatedId.findOne({})
+            return result
+        } catch (e) {
+            console.log(e)
+        } finally {
+            await client.close()
+        }
     }
-}
 
-async function updateIdFromDatabase(client, updatedId) {
-    const id = updatedId.updatedId
-    try {
-        await client.connect()
-        const result = await generatedId.updateOne({}, { $set: { id: id } })
-        return result
-    } catch (e) {
-        console.log(e)
-    } finally {
-        await client.close()
+    async function updateIdFromDatabase(client, updatedId) {
+        const id = updatedId.updatedId
+        try {
+            await client.connect()
+            const result = await generatedId.updateOne({}, { $set: { id: id } })
+            return result
+        } catch (e) {
+            console.log(e)
+        } finally {
+            await client.close()
+        }
     }
-}
 
-async function findUserInDatabase(client, loginData) {
-    const { login, password } = loginData
-    try {
-        await client.connect()
-        const result = await usersCollection.findOne({ login: login, password: password })
-        return result
-    } catch (e) {
-        console.error(e)
-    } finally {
-        await client.close()
+    async function findUserInDatabase(client, loginData) {
+        const { login, password } = loginData
+        try {
+            await client.connect()
+            const result = await usersCollection.findOne({ login: login, password: password })
+            return result
+        } catch (e) {
+            console.error(e)
+        } finally {
+            await client.close()
+        }
     }
-}
-
-
-module.exports = MongoClient
-module.exports = {
-    findUserInDatabase: findUserInDatabase,
-    updateIdFromDatabase: updateIdFromDatabase,
-    getIdFromDatabase: getIdFromDatabase,
-    getListOfUsers: getListOfUsers,
-    checkIfUserExists: checkIfUserExists,
-    addNewTaskToDatabase: addNewTaskToDatabase,
-    registerNewUser: registerNewUser,
-    getListOfTasks: getListOfTasks,
-    updateTaskStatus: updateTaskStatus,
-    deleteTask: deleteTask
+    module.exports = MongoClient
+    module.exports = {
+        findUserInDatabase: findUserInDatabase,
+        updateIdFromDatabase: updateIdFromDatabase,
+        getIdFromDatabase: getIdFromDatabase,
+        getListOfUsers: getListOfUsers,
+        checkIfUserExists: checkIfUserExists,
+        addNewTaskToDatabase: addNewTaskToDatabase,
+        registerNewUser: registerNewUser,
+        getListOfTasks: getListOfTasks,
+        updateTaskStatus: updateTaskStatus,
+        deleteTask: deleteTask
+    }
+} else {
+    console.log('błąd')
 }
--- src/scripts/server/server.js
+++ src/scripts/server/server.js
@@ -11,82 +11,93 @@ app.use(cors());
 app.use(express.json())
 
 
-const uri = 'mongodb+srv://olek:zaqwsxcde@app.t3wuhzm.mongodb.net/?retryWrites=true&w=majority'
-const { MongoClient } = require('mongodb')
-const client = new MongoClient(uri)
+const DB_USERNAME = process.env.DB_USERNAME
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_URL = process.env.DB_URL
 
-const {
-    getListOfTasks,
-    registerNewUser,
-    addNewTaskToDatabase,
-    getListOfUsers,
-    checkIfUserExists,
-    updateTaskStatus,
-    getIdFromDatabase,
-    updateIdFromDatabase,
-    findUserInDatabase,
-    deleteTask
-} = require('./DB/index.js')
+if (DB_USERNAME && DB_PASSWORD && DB_URL) {
+    const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_URL}`
 
+    const { MongoClient } = require('mongodb')
+    const client = new MongoClient(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
 
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
-
-app.get('/users', async (req, res) => {
-    const USERS_LIST = await getListOfUsers(client)
-    res.send(USERS_LIST)
-})
-
-app.post('/tasks', async (req, res) => {
-    console.log('adding new task')
-    const NEW_TASK = req.body
-    const NEW_TASKS_LIST = await addNewTaskToDatabase(client, NEW_TASK)
-    res.send(NEW_TASKS_LIST)
-})
-
-app.post('/update-task', async (req, res) => {
-    const UPDATED_TASK = req.body
-    const UPDATED_TASKS_LIST = await updateTaskStatus(client, UPDATED_TASK)
-    res.send(UPDATED_TASKS_LIST)
-})
-
-app.post('/delete-task', async (req, res) => {
-    const TASK_TO_DELETE = req.body
-    const UPDATED_TASKS_LIST = await deleteTask(client, TASK_TO_DELETE)
-    res.send(UPDATED_TASKS_LIST)
-})
-
-app.get('/get-tasks', async (req, res) => {
-    const TASKS_LIST = await getListOfTasks(client)
-    res.send(TASKS_LIST)
-})
+    const {
+        getListOfTasks,
+        registerNewUser,
+        addNewTaskToDatabase,
+        getListOfUsers,
+        checkIfUserExists,
+        updateTaskStatus,
+        getIdFromDatabase,
+        updateIdFromDatabase,
+        findUserInDatabase,
+        deleteTask
+    } = require('./DB/index.js');
 
 
-app.get('/get-id', async (req, res) => {
-    const generatedId = await getIdFromDatabase(client)
-    res.send(generatedId)
-})
 
-app.post('/update-id', async (req, res) => {
-    res.send(req.body)
-    const updatedId = req.body
-    await updateIdFromDatabase(client, updatedId)
-})
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`)
+    })
 
-app.post('/sign-in', async (req, res) => {
-    const loginData = req.body
-    const result = await findUserInDatabase(client, loginData)
-    res.send(result)
-})
 
-app.post('/register', async (req, res) => {
-    const NEW_USER_TO_REGISTER = req.body
-    const EXISTING_USER = await checkIfUserExists(client, NEW_USER_TO_REGISTER)
-    res.send(EXISTING_USER)
-    if (EXISTING_USER) return
-    await registerNewUser(client, NEW_USER_TO_REGISTER)
-})
+    app.get('/users', async (req, res) => {
+        const USERS_LIST = await getListOfUsers(client)
+        res.send(USERS_LIST)
+    })
 
+    app.post('/tasks', async (req, res) => {
+        console.log('adding new task')
+        const NEW_TASK = req.body
+        const NEW_TASKS_LIST = await addNewTaskToDatabase(client, NEW_TASK)
+        res.send(NEW_TASKS_LIST)
+    })
+
+    app.post('/update-task', async (req, res) => {
+        const UPDATED_TASK = req.body
+        const UPDATED_TASKS_LIST = await updateTaskStatus(client, UPDATED_TASK)
+        res.send(UPDATED_TASKS_LIST)
+    })
+
+    app.post('/delete-task', async (req, res) => {
+        const TASK_TO_DELETE = req.body
+        const UPDATED_TASKS_LIST = await deleteTask(client, TASK_TO_DELETE)
+        res.send(UPDATED_TASKS_LIST)
+    })
+
+    app.get('/get-tasks', async (req, res) => {
+        const TASKS_LIST = await getListOfTasks(client)
+        res.send(TASKS_LIST)
+    })
+
+
+    app.get('/get-id', async (req, res) => {
+        const generatedId = await getIdFromDatabase(client)
+        res.send(generatedId)
+    })
+
+    app.post('/update-id', async (req, res) => {
+        res.send(req.body)
+        const updatedId = req.body
+        await updateIdFromDatabase(client, updatedId)
+    })
+
+    app.post('/sign-in', async (req, res) => {
+        const loginData = req.body
+        const result = await findUserInDatabase(client, loginData)
+        res.send(result)
+    })
+
+    app.post('/register', async (req, res) => {
+        const NEW_USER_TO_REGISTER = req.body
+        const EXISTING_USER = await checkIfUserExists(client, NEW_USER_TO_REGISTER)
+        res.send(EXISTING_USER)
+        if (EXISTING_USER) return
+        await registerNewUser(client, NEW_USER_TO_REGISTER)
+    })
+} else {
+    console.log('Missing environment variables');
+}
