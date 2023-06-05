517085d326f65d7c67bf95cec6e7cebec53a265b
akopton <kopton.olek@gmail.com>
2023-06-05 10:57:12 +0200

    finished app

--- src/scripts/front/components/SingleTask.tsx
+++ src/scripts/front/components/SingleTask.tsx
@@ -3,52 +3,66 @@ import useConvertedDate from "../methods/useConvertedDate"
 import { TTask } from "../interfaces/taskInterface"
 import { updateTask } from "../methods/updateTask"
 
-const SingleTask = ({ task, setTaskToOpen, setIsTaskOpened, searchValue, setTasks, windowWidth }:{ task:TTask, setTaskToOpen:any, setIsTaskOpened:any, searchValue:string, setTasks:any, windowWidth:number }) => {
-    const [isExpired, setIsExpired] = useState<boolean>(false)
-    const convertedDate = useConvertedDate(task.date, searchValue)
-    const {title} = task
-    const handleTaskToOpen = (task: TTask) => {
-        setIsTaskOpened(true)
-        setTaskToOpen(task)
-    }
+const SingleTask = ({
+  task,
+  setTaskToOpen,
+  setIsTaskOpened,
+  searchValue,
+  setTasks,
+  windowWidth,
+}: {
+  task: TTask
+  setTaskToOpen: any
+  setIsTaskOpened: any
+  searchValue: string
+  setTasks: any
+  windowWidth: number
+}) => {
+  const [isExpired, setIsExpired] = useState<boolean>(false)
+  const convertedDate = useConvertedDate(task.date, searchValue)
+  const { title } = task
+  const handleTaskToOpen = (task: TTask) => {
+    setIsTaskOpened(true)
+    setTaskToOpen(task)
+  }
 
-    const handleExpiredTask = async () => {
+  const handleExpiredTask = async () => {
     const todaysDate = new Date().getTime()
-    
-        if (todaysDate > task.date) {
-            if (task.status !== 'expired' && task.status !== 'done') {
-                await updateTask(task, 'expired')
-                    .then((data:any) => data.json())
-                    .then((res:TTask[]) => {
-                        setTasks(res)
-                    })
-            }
-            setIsExpired(true)
-        }
-        else setIsExpired(false)
-    }
 
-    useEffect(()=>{
-        handleExpiredTask()
-    },[isExpired])
+    if (todaysDate > task.date) {
+      if (task.status !== "expired" && task.status !== "done") {
+        await updateTask(task, "expired")
+          .then((data: any) => data.json())
+          .then((res: TTask[]) => {
+            setTasks(res)
+          })
+      }
+      setIsExpired(true)
+    } else setIsExpired(false)
 
-    return (
-        <li
-            className={isExpired ? 'single-task expired' : 'single-task'}
-            onClick={() => handleTaskToOpen(task)}
-        >
-            <span className="single-task__id">{task.innerId}</span>
-            <span className="single-task__title">{title}</span>
-            <div className="single-task__bottom-wrap">
-                    <span className="task-content__asignee">
-                        {task.asignee.firstName} {task.asignee.lastName}
-                    </span>
-                    <span className="task-content__date">
-                        {convertedDate}
-                    </span>
-            </div>
-        </li>
-    )
+    if (todaysDate > task.date && task.status === "done")
+      setIsExpired(false)
+  }
+
+  useEffect(() => {
+    handleExpiredTask()
+  }, [isExpired])
+
+  return (
+    <li
+      className={isExpired ? "single-task expired" : "single-task"}
+      onClick={() => handleTaskToOpen(task)}
+    >
+      <span className="single-task__id">{task.innerId}</span>
+      <span className="single-task__title">{title}</span>
+      <div className="single-task__bottom-wrap">
+        <span className="task-content__asignee">
+          {task.asignee.firstName} {task.asignee.lastName}
+        </span>
+        <span className="task-content__date">{convertedDate}</span>
+      </div>
+    </li>
+  )
 }
 
 export default SingleTask
--- src/scripts/front/components/UserItem.tsx
+++ src/scripts/front/components/UserItem.tsx
@@ -6,76 +6,139 @@ import { deleteUser } from "../methods/deleteUser"
 import { updateUser } from "../methods/updateUser"
 import { DeleteUserWindow } from "./DeleteUserWindow"
 
-export const UserItem = ({user, tasks, setUsers}: {user: TUser, tasks:TTask[], setUsers:any}) => {
-    const [showRoleSelection, setShowRoleSelection] = useState<boolean>(false)
-    const [showDeleteWindow, setShowDeleteWindow] = useState<boolean>(false)
+export const UserItem = ({
+  user,
+  tasks,
+  setUsers,
+}: {
+  user: TUser
+  tasks: TTask[]
+  setUsers: any
+}) => {
+  const [showRoleSelection, setShowRoleSelection] =
+    useState<boolean>(false)
+  const [showDeleteWindow, setShowDeleteWindow] =
+    useState<boolean>(false)
 
-    const getTasksCountForUser = (user: TUser, status: string) => {
-        switch (status) {
-            case 'all':
-                return tasks.filter(task => task.asignee._id === user._id && task.status !== 'deleted').length
-            case 'todo':
-                return tasks.filter(task => task.asignee._id === user._id && task.status !== 'deleted' && task.status === status).length
-            case 'active':
-                return tasks.filter(task => task.asignee._id === user._id && task.status !== 'deleted' && task.status === status).length
-            case 'done':
-                return tasks.filter(task => task.asignee._id === user._id && task.status !== 'deleted' && task.status === status).length
-            case 'expired':
-                return tasks.filter(task => task.asignee._id === user._id && task.status !== 'deleted' && task.status === status).length
-        }
+  const getTasksCountForUser = (user: TUser, status: string) => {
+    switch (status) {
+      case "all":
+        return tasks.filter(
+          (task) =>
+            task.asignee._id === user._id && task.status !== "deleted"
+        ).length
+      case "todo":
+        return tasks.filter(
+          (task) =>
+            task.asignee._id === user._id &&
+            task.status !== "deleted" &&
+            task.status === status
+        ).length
+      case "active":
+        return tasks.filter(
+          (task) =>
+            task.asignee._id === user._id &&
+            task.status !== "deleted" &&
+            task.status === status
+        ).length
+      case "done":
+        return tasks.filter(
+          (task) =>
+            task.asignee._id === user._id &&
+            task.status !== "deleted" &&
+            task.status === status
+        ).length
+      case "expired":
+        return tasks.filter(
+          (task) =>
+            task.asignee._id === user._id &&
+            task.status !== "deleted" &&
+            task.status === status
+        ).length
     }
+  }
 
-
-    const deleteUserFromDatabase = async (user: TUser) => {
-        if (user && user._id) {
-            const result = await deleteUser(user._id)
-            const newData:TUser[] = await result.json()
-            setUsers(newData)
-            setShowDeleteWindow(false)
-        }
+  const deleteUserFromDatabase = async (user: TUser) => {
+    if (user && user._id) {
+      const result = await deleteUser(user._id)
+      const newData: TUser[] = await result.json()
+      setUsers(newData)
+      setShowDeleteWindow(false)
     }
+  }
 
-    const setUserRole = async (user: TUser) => {
-        const result = await updateUser(user)
-        const newData:TUser[] = await result.json()
-        setUsers(newData)
-    }
+  const setUserRole = async (user: TUser) => {
+    const result = await updateUser(user)
+    const newData: TUser[] = await result.json()
+    setUsers(newData)
+  }
 
-    const handleUserRole = (user: TUser, role: string) => {
-        user.role = role
-        setShowRoleSelection(false)
-        setUserRole(user)
-    }
+  const handleUserRole = (user: TUser, role: string) => {
+    user.role = role
+    setShowRoleSelection(false)
+    setUserRole(user)
+  }
 
-    return (
-        <div 
-            key={user._id}
-            id={user.login}
-            className="selected-user"
+  return (
+    <div key={user._id} id={user.login} className="selected-user">
+      <ul className="details-list">
+        <li className="details-list__item">
+          Zadania użytkownika: {getTasksCountForUser(user, "all")}
+        </li>
+        <li className="details-list__item">
+          Do zrobienia: {getTasksCountForUser(user, "todo")}
+        </li>
+        <li className="details-list__item">
+          W trakcie: {getTasksCountForUser(user, "active")}
+        </li>
+        <li className="details-list__item">
+          Zrobione: {getTasksCountForUser(user, "done")}
+        </li>
+        <li className="details-list__item">
+          Przeterminowane: {getTasksCountForUser(user, "expired")}
+        </li>
+      </ul>
+      <div className="user-role">
+        Uprawnienia:{" "}
+        {user.role === "user" ? "użytkownik" : "administrator"}
+      </div>
+      <div className="buttons-wrapper">
+        <button
+          className="btn form__btn"
+          onClick={() => setShowRoleSelection(true)}
         >
-            <ul className="details-list">
-                <li className="details-list__item">Zadania użytkownika: {getTasksCountForUser(user, 'all')}</li>
-                <li className="details-list__item">Do zrobienia: {getTasksCountForUser(user, 'todo')}</li>
-                <li className="details-list__item">W trakcie: {getTasksCountForUser(user, 'active')}</li>
-                <li className="details-list__item">Zrobione: {getTasksCountForUser(user, 'done')}</li>
-                <li className="details-list__item">Przeterminowane: {getTasksCountForUser(user, 'expired')}</li>
-            </ul>
-            <div className="user-role">Uprawnienia: {user.role === 'user' ? 'użytkownik' : 'administrator'}</div>
-            <div className="buttons-wrapper">
-                <button className="btn form__btn" onClick={()=>setShowRoleSelection(true)}>Nadaj uprawnienia</button>
-                <button className="btn form__btn" onClick={()=>setShowDeleteWindow(true)}>Usuń użytkownika</button>
-            </div>
-            {
-                showRoleSelection &&
-                <div className="role-selection">
-                    <button className="btn form__btn" onClick={() => handleUserRole(user, 'user')}>Użytkownik</button>
-                    <button className="btn form__btn" onClick={() => handleUserRole(user, 'admin')}>Administrator</button>
-                </div>
-            }
-            {
-                showDeleteWindow &&
-                <DeleteUserWindow setShowDeleteWindow={setShowDeleteWindow} handleDeleteAccount={()=>deleteUserFromDatabase(user)}/>
-            }
+          Nadaj uprawnienia
+        </button>
+        <button
+          className="btn form__btn"
+          onClick={() => setShowDeleteWindow(true)}
+        >
+          Usuń użytkownika
+        </button>
+      </div>
+      {showRoleSelection && (
+        <div className="role-selection">
+          <button
+            className="btn form__btn"
+            onClick={() => handleUserRole(user, "user")}
+          >
+            Użytkownik
+          </button>
+          <button
+            className="btn form__btn"
+            onClick={() => handleUserRole(user, "admin")}
+          >
+            Administrator
+          </button>
         </div>
-    )
+      )}
+      {showDeleteWindow && (
+        <DeleteUserWindow
+          setShowDeleteWindow={setShowDeleteWindow}
+          handleDeleteAccount={() => deleteUserFromDatabase(user)}
+          user={user}
+        />
+      )}
+    </div>
+  )
 }
